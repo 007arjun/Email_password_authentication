1cf2618093b5c0518e66317fb3404e8f5e41561c
Arjun007wolf <anandarjun2000@gmail.com>
2023-04-14 10:17:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/arben_auth/Mainapge.java
@@ -0,0 +1,14 @@
+package com.example.arben_auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mainapge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainapge);
+    }
+}
